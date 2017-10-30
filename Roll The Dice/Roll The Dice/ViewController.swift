d603b25a2e2892d9c1600c07049d864162c46bf0
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-30 11:57:02 +0000

    Added array of possible dice throws

--- Roll The Dice/Roll The Dice/ViewController.swift
+++ Roll The Dice/Roll The Dice/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Vars
+    var randomDiceIndex1 = 0
+    var randomDiceIndex2 = 0
+    
+    //Array
+    let diceArr = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
     //Outlets
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
@@ -27,6 +34,14 @@ class ViewController: UIViewController {
 
     
     @IBAction func rollBtnPressed(_ sender: Any) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArr[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArr[randomDiceIndex2])
         
     }
     
