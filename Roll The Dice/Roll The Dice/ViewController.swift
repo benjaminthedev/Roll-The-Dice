c19608faca754c71d26f2e2a2bb94f4ec83c6282
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-30 09:13:25 +0000

    Added assets

--- Roll The Dice/Roll The Dice/ViewController.swift
+++ Roll The Dice/Roll The Dice/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func rollBtnPressed(_ sender: Any) {
+        
+    }
+    
 
 }
 
